7263f8408f95206c415010a3568eb64afe4d40f1
Rameez Ibrahim <115018565+RameezIbrahimOfficial@users.noreply.github.com>
2022-12-01 16:42:08 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,12 @@
+let v = 0;
+function addBtn(){
+    v += 1;
+    document.getElementById('labelvalue').innerText=v;
+    
+}
+
+function subBtn(){
+    v -= 1;
+    document.getElementById('labelvalue').innerText=v;
+  
+}
